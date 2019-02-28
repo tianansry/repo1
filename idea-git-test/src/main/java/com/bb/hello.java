b0f998f66d4d6d88b91535364fad33f448f1405b
Tian <zxt19952007@126.com>
2019-02-28 18:49:00 +0800

    添加工程到本地

---
+++ idea-git-test/src/main/java/com/bb/hello.java
@@ -0,0 +1,9 @@
+package com.bb;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
